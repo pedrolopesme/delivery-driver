d226fd835113733f5c20eba4c0913dad28c13015
pedro.mendes <pedro.mendes@g.globo>
2021-10-23 18:15:10 -0300

    minor: add colliders to car and some obstacle

--- Assets/Scenes/Driver.cs
+++ Assets/Scenes/Driver.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    float steerSpeed= 1f;
-    float moveSpeed= 0.001f;
+    float steerSpeed= 200f; 
+    float moveSpeed= 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,8 +15,8 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
-        float movementAmount = Input.GetAxis("Vertical") * moveSpeed;
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float movementAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, movementAmount, 0); 
